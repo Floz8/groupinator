d4ed41de5ca3de62ec9747b8b1eb58f108464c04
Floz8 <55977382+Floz8@users.noreply.github.com>
2022-12-13 09:57:23 +0100

    creation liste

--- index.php
+++ index.php
@@ -6,9 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Groupinator</title>
-</head>
-<body>
-    
+
     <?php 
    class Personne {
 
@@ -37,13 +35,31 @@
    
 
     ?>
+</head>
+<body>
+    
+   
 
     <p> <?php
+
+     $count=0;
      $p1 = new Personne("Martin", "Dupont");
      $p2= new Personne("Philippe", "Durand");
      $p3 = new Personne("John", "Doe");
      $p4 = new Personne("Alice","Bob");
+
+     $liste_all=[$p1,$p2,$p3,$p4];
+     shuffle($liste_all);
+     foreach($liste_all as $personne){
+     echo($personne->getFirstName());
+     $count++; 
+     }
+     echo $count;
+     ?>
+
      
-     echo($p1->getFirstName()); ?>
+
+
+
 </body>
 </html>
